4ce53d928d896e798acc96986add263271406de8
alejandro-santos <49452035+alejandro-santos@users.noreply.github.com>
2022-05-06 15:47:56 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -40,12 +40,18 @@ function sendApiRequest() {
       songTitle = title[i].result.title;
       songArtist = title[i].result.artist_names;
       songViews = title[i].result.stats.pageviews.toLocaleString('en-US');
+        hotStatus = title[i].result.stats.hot;
+      if (hotStatus == true) {
+        hotStatus = ' <i class="fa-solid fa-fire-flame-curved"></i>'
+      } else {
+        hotStatus = ""
+      }
       ranking = i + 1;
 
       //type = "<li> <a href='" + lyrics + "'><img src='" + title[i].result.song_art_image_url + "'></a></li>";
       type =  "<div id='card' > <img id='album-cover'  alt='" + songId + "' src='" 
       + songUrl + "'> <p id='song-title'>" 
-      + ranking + ". " + songTitle + "</p> <p class='artist-names'>" 
+      + ranking + ". " + songTitle + hotStatus + "</p>" + "<p class='artist-names'>" 
       + songArtist + "</p>"
       + "<p id='page-views'> " + songViews + " <span style='color:black;'>page views</span> </p> </div>";
       document.getElementById("song-card-list").innerHTML += type;
@@ -71,22 +77,36 @@ $(document).ready(function(){
     artist = record.response.song.artist_names;
     player = record.response.song.apple_music_player_url;
     albumCover = record.response.song.song_art_image_url;
-    window.close();
+    artistNames = record.response.song.primary_artist.image_url;
+    links = "";
+    for (var i = 0; i < record.response.song.media.length; i ++) {
+      media = record.response.song.media[i].url;
+      if(media.indexOf('spotify') >= 0){
+        links += "<li id='medias'> <a target='_blank' href='" + media + "'> <i class='fa-brands fa-spotify'></i> </a> </li>";
+      }
+      if(media.indexOf('youtube') >= 0){
+        links += "<li id='medias'> <a target='_blank' href='" + media + "'> <i class='fa-brands fa-youtube'></i> </a> </li>";
+      }
+      if(media.indexOf('soundcloud') >= 0){
+        links += "<li id='medias'> <a target='_blank' href='" + media + "'> <i class='fa-brands fa-soundcloud'></i> </a> </li>";
+      }
+    }
     var tab = window.open();
     tab.document.write('<title>GENIUS</title>' 
     + '<script src="https://kit.fontawesome.com/6c5e78ae37.js" crossorigin="anonymous"></script>' 
     + '<link rel="icon" type="image/x-icon" href="/GENIUS/images/favicon.ico">' 
-    + '<link rel="stylesheet" href="/GENIUS/css/main.css">' 
-    + '<h1 id="close"> <i class="fa-solid fa-arrow-left-long"></i><a href="https://alejandro-santos.github.io/GENIUS/">New Search</a></h1>'
+    + '<link rel="stylesheet" href="/css/main.css">' 
+    + '<h1 id="close">Close Tab</h1>'
+    + '<div id="all"> <div id="main-stuff"> <div id="artist-frame"> <img id="artist-picture" src="' + artistNames + '"> </div>'
     + '<h1 id="artist-names">' + title + '</h1>'
     + '<p id="song">' + artist + '</p>'
-    + '<div id="album-frame"> <img id="album-picture" src="' + albumCover + '"></div>'
-    + '<div id="frame"> <iframe src="' + player + '" title=""></iframe> </div>'
-    + '<div id="container"><div id="song-card-list"> </div> </div> '
+    + '<div id="album-frame"> <img id="album-picture" src="' + albumCover + '"> </div>'
+    + '<div id="media-list"> <ul>' + links + '</ul></div>'
+    + '<div id="frame"> <iframe src="' + player + '" title=""></iframe> </div> </div>'
+    +  '<div id="song-lyrics">' + lyrics + '</div> </div>'
     + ' <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>'
-    + '<script src="/GENIUS/js/script.js"></script>'
-    + '<script src="/GENIUS/js/result.js"></script>'
-    +  lyrics);
+    + '<script src="/js/script.js"></script>'
+    + '<script src="/js/result.js"></script>');
     tab.document.close(); 
   };
 
